25a7b9c7439e350497910a3ce5c62f728568f2df
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-03-25 22:45:12 +0600

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { routingComponents} from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    DepartmentOverviewComponent,
+    DepartmentContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
